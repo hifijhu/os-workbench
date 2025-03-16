828ca1826563abb211d830a77fbe7792a3f300c9
tracer-nju <tracer@nju.edu.cn>
2025-03-16 21:43:05 +0800

    Linux hifi-VirtualBox 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:43:05 up  4:38,  1 user,  load average: 0.81, 1.24, 1.52

---
+++ .shadow/pstree/pstree.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <assert.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
+#include <node.h>
+#include <tree.h>
+
+void print_func(struct tree* t, struct Node* node, int depth) {
+  for (int i = 0; i < depth; i++) {
+    printf("  ");
+  }
+  printf("name: %s", node->name);
+  for(int i = 0; i < node->cid_size; i++) {
+    print_func(t, &t->nodes[node->cid[i]], depth + 1);
+  }
+}
+int main(int argc, char *argv[]) {
+  struct tree* t = malloc(sizeof(struct tree));
+  t->tree_size = 0;
+  t->tree_capacity = 1024;
+  t->nodes = malloc(sizeof(struct Node) * t->tree_capacity);
+  assert(t->nodes);
+
+  for (int i = 0; i < argc; i++) {
+    assert(argv[i]);
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  assert(!argv[argc]);
+  
+  struct tree* t = malloc(sizeof(struct tree));
+  t->tree_size = 0;
+  t->tree_capacity = 1024;
+  t->nodes = malloc(sizeof(struct Node) * t->tree_capacity);
+  assert(t->nodes);
+  // 用malloc等函数分配内存
+
+  char name[256];
+  DIR* dp;
+  char* path = "/proc";
+  if ((dp == opendir(path)) == NULL){
+    fprintf(stderr, "error happend: %s\n", path);
+    return 0;
+  }
+
+  struct dirent* stp;
+
+  while ((stp = readdir(dp)) != NULL){
+    if (strcmp(stp->d_name, ".") == 0 || strcmp(stp->d_name, "..") == 0)
+      continue;
+    if (strlen(path) + strlen(stp->d_name) + 2 > sizeof(name)) {
+      fprintf(stderr, "fetchdir name %s %s too long\n", path, stp->d_name);
+    } 
+    else {
+        int tindex;
+        if (stp->d_name[0] >= '0' && stp->d_name[0] <= '9') {
+          tindex = atoi(stp->d_name);
+        }
+        else {
+          continue;
+        }
+        char* filename = "/status";
+        FILE *fp = fopen(strcat(strcat(strcat(path, "/"), stp->d_name), filename), "r");
+        if (!fp) goto release;
+
+        char line[256];
+        while (fgets(line, sizeof(line), fp)) {
+          if (tindex >= t->tree_capacity) {
+            t->tree_capacity *= 2;
+            t->nodes = realloc(t->nodes, sizeof(struct Node) * t->tree_capacity);
+            assert(t->nodes);
+          }
+          struct Node* node = &t->nodes[tindex];
+          if (strncmp(line, "Name:", 5) == 0) {
+            strcpy(node->name, line + 5);
+          }
+          if (strncmp(line, "Pid:", 4) == 0) {
+            strcpy(node->pid, line + 4);
+          }
+          if (strncmp(line, "PPid:", 5) == 0) {
+            strcpy(node->ppid, line + 5);
+            struct Node* parent = &t->nodes[atoi(node->ppid)];
+            parent->cid[parent->cid_size++] = tindex;
+            t->tree_size++;
+            break;
+          }
+        }
+        // 用fscanf, fgets等函数读取
+
+      release:
+        if (fp) fclose(fp);
+    }
+  }
+  closedir(dp);
+
+
+
+  return 0;
+}
