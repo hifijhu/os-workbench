5f035e0374325084e29a47dc2a28e22605506341
tracer-nju <tracer@nju.edu.cn>
2025-03-19 04:31:59 +0800

    Linux hifi-VirtualBox 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  04:31:59 up 49 min,  1 user,  load average: 0.88, 1.03, 1.10

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 #include <dirent.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include "tree.h"
 
-void print_func(struct tree* t, struct Node* node, int depth) {
+void print_func(struct tree* t, struct Node* node, int depth, bool p) {
   for (int i = 0; i < depth; i++) {
     printf("  ");
   }
-  printf("%s", node->name);
+  printf("%s(%d)", node->name, node->pid);
   for(int i = 0; i < node->cid_size; i++) {
-    print_func(t, &t->nodes[node->cid[i]], depth + 1);
+    print_func(t, &t->nodes[node->cid[i]], depth + 1, p);
   }
 }
 int main(int argc, char *argv[]) {
 
   for (int i = 0; i < argc; i++) {
+
     assert(argv[i]);
     printf("argv[%d] = %s\n", i, argv[i]);
   }
+  bool n, p, v;
+  n = p = v = false;
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--numeric-sort") == 0) {
+      n = true;
+    }
+    else if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--show-pids") == 0) {
+      p = true;
+    }
+    else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) {
+      v = true;
+    }
+  }
   assert(!argv[argc]);
-  
+  if (v == true) {
+    printf("pstree (PSmisc) 23.7\nCopyright © 1993-2024 Werner Almesberger 和 Craig Small\n\nPSmisc 不提供任何保证。\n该程序为自由软件，欢迎你在 GNU 通用公共许可证 (GPL) 下重新发布。\n详情可参阅 COPYING 文件。\n");
+    return 0;
+  }
   struct tree* t = malloc(sizeof(struct tree));
   t->tree_size = 0;
   t->tree_capacity = 1024;
