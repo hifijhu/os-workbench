9f31210b45b9d7751e63fa6416d94d20a87b5162
tracer-nju <tracer@nju.edu.cn>
2025-03-20 21:24:08 +0800

    Linux hifi-VirtualBox 6.8.0-49-generic #49-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov  4 02:06:24 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:24:08 up  7:04,  1 user,  load average: 3.47, 4.12, 2.91

--- .shadow/abstract-machine/klib/src/string.c
+++ .shadow/abstract-machine/klib/src/string.c
@@ -65,15 +65,13 @@ char *strcat(char *dst, const char *src) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("strcmp");
-      /*
+
   while(*s1 == *s2 && *s1 != '\0' && *s2 != '\0') {
     s1++;
     s2++;
   }
   return *s1 - *s2;
 
-       */
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
