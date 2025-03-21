4396e45c7746862bf3d0aac776767f10eeff6ec2
tracer-nju <tracer@nju.edu.cn>
2025-03-21 15:46:30 +0800

    Linux hifi-VirtualBox 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:46:30 up  2:52,  1 user,  load average: 4.94, 5.24, 5.34

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -7529,7 +7529,7 @@ int main(const char *args) {
   puts("mainargs = \"");
   puts(args);  // make run mainargs=xxx
   puts("\"\n");
-  paint();
+  splash();
   while (1) {
     /*
     AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
