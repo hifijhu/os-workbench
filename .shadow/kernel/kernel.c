6bb8b1f9e952b26e7b109c1302819ae773a72050
tracer-nju <tracer@nju.edu.cn>
2025-03-21 15:12:52 +0800

    Linux hifi-VirtualBox 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:12:52 up  2:18,  1 user,  load average: 4.80, 2.86, 2.17

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -7529,13 +7529,13 @@ int main(const char *args) {
   puts("\"\n");
   splash();
   while (1) {
-
+    /*
     AM_INPUT_KEYBRD_T event = { .keycode = AM_KEY_NONE };
     ioe_read(AM_INPUT_KEYBRD, &event);
     if (event.keycode != AM_KEY_NONE && event.keydown) {
       if (strcmp(key_names[event.keycode], "ESCAPE") == 0) break;
     }
-
+    */
   }
   //halt(0);
   return 0;
