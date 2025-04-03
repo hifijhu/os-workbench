2f06f65fd98fd41f8ec1f47a623df3e299f80c0c
tracer-nju <tracer@nju.edu.cn>
2025-04-03 17:14:30 +0800

    Linux hifi-VirtualBox 6.11.0-19-generic #19~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 17 11:51:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:14:30 up  3:01,  1 user,  load average: 0.55, 0.35, 0.17

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -3,8 +3,88 @@
 #include <kernel.h>
 #include <klib.h>
 
+#include "ctrlblk.h"
+enum ops { OP_ALLOC = 1, OP_FREE };
+
+struct malloc_op {
+    enum ops type;
+    union {
+        size_t sz;  // OP_ALLOC: size
+        void *addr; // OP_FREE: address
+    };
+};
+
+struct op_list{
+    struct op_list* next;
+    void* ptr;
+};
+
+typedef struct{
+    struct op_list* next;
+    size_t size;
+}op_header;
+
+op_header header;
+
+static void* op_gain(){
+    void* ptr = header.next->ptr;
+    header.next = header.next->next;
+    return ptr;
+}
+static struct malloc_op random_op(){
+    struct malloc_op op;
+    int rd = rand();
+    if (rd % 2 == 0){
+        op.type = OP_ALLOC;
+        op.sz = rd;
+    }
+    else{
+        op.type = OP_FREE;
+        op.addr = op_gain();
+    }
+    return op;
+}
+
+static void alloc_check(void* ptr, size_t sz){
+    if (ptr){
+        struct ctrlblk* sptr = ptr - sizeof(struct ctrlblk);
+        if (sptr->size == sz){
+            struct op_list node;
+            node.next = header.next;
+            node.ptr = ptr;
+            header.next = &node;
+            header.size++;
+        }
+        else{
+            assert(1 == 0);
+        }
+    }
+    else{
+        assert(0 == 1);
+    }
+}
+void stress_test() {
+    while (1) {
+        // 根据 workload 生成操作
+        struct malloc_op op = random_op();
+
+        switch (op.type) {
+            case OP_ALLOC: {
+                void *ptr = pmm->alloc(op.sz);
+                alloc_check(ptr, op.sz);
+                break;
+            }
+            case OP_FREE:
+                pmm->free(op.addr);
+                break;
+        }
+    }
+}
+
 int main() {
     os->init();
-    mpe_init(os->run);
+    header.next = NULL;
+    header.size = 0;
+    mpe_init(stress_test);
     return 1;
 }
