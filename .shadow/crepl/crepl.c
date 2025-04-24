14fec313448a68868de139509350c8f51fe451d5
tracer-nju <tracer@nju.edu.cn>
2025-04-24 20:58:34 +0800

    Linux hifi-VirtualBox 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:34 up 12:56,  1 user,  load average: 0.15, 0.15, 0.32

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[]) {
         snprintf(exec_name, sizeof(exec_name), "%s", temp);
         snprintf(exec_path, sizeof(exec_path), "./tmp/%s", temp);
         snprintf(new_name, sizeof(new_name), "%s.c", temp);
-        if (rename(new_name, temp) != 0){
+        if (rename(temp, new_name) != 0){
             perror("rename");
             close(fd);
             return 1;
