4375e7f63922cfdb3dfc0bd8503a64879a43498f
tracer-nju <tracer@nju.edu.cn>
2025-04-24 20:08:31 +0800

    Linux hifi-VirtualBox 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:08:31 up 12:06,  1 user,  load average: 0.37, 0.63, 1.14

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <unistd.h>
+#include <wait.h>
 int main(int argc, char *argv[]) {
     static char line[4096];
-
+    int count = 0;
     while (1) {
         printf("crepl> ");
         fflush(stdout);
@@ -11,8 +13,55 @@ int main(int argc, char *argv[]) {
         if (!fgets(line, sizeof(line), stdin)) {
             break;
         }
+        
 
         // To be implemented.
-        printf("Got %zu chars.\n", strlen(line));
+        char *temp = "/tmp/XXXXXX.c";
+        int fd = mkstemp(temp);
+        if (fd == -1){
+            perror("mkstemp");
+            close(fd);
+            return 1;
+        }
+
+        write(fd, line , sizeof(line));
+
+        char new_name[256];
+        char exec_file[256];
+        snprintf(exec_file, sizeof(exec_file), "./%s", temp);
+        snprintf(new_name, sizeof(new_name), "%s.c", temp);
+        if (rename(new_name, temp) != 0){
+            perror("rename");
+            close(fd);
+            return 1;
+        }
+
+        close(fd);
+
+        int pid = fork();
+        if (pid == 0){
+            execlp("gcc", temp, "-o", exec_file);
+        } else if (pid > 0){
+            int status;
+            waitpid(pid, &status, 0);
+        }
+        else{
+            perror("fork");
+            return 1;
+        }
+
+        int ppid = fork();
+        if (ppid = 0){
+            execv(exec_file, "");
+
+        } else if (ppid > 0){
+            int sstatus;
+            waitpid(ppid, &sstatus, 0);
+        } else {
+            perror("ppid");
+            return 1;
+        }
+
+        count++;
     }
 }
